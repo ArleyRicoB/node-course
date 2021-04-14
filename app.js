f9d884f38fabf449bf438dd63884403b406d0024
Arley Sneyder Rico Bohórquez <arleyrc09@gmail.com>
2021-04-14 17:29:24 -0500

    Routing requests

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-	console.log(req);
+	const url = req.url;
+
+	if(url === '/') {
+		res.write('<html>');
+		res.write('<head><title>Enter message</title></head>');
+		res.write('<body><form action="/message" method="POST" name="message"><input type="text"><button type="submit">Send</button></form></body>');
+		res.write('</html>');
+		return res.end();
+	}
 
 	res.setHeader('Content-Type', 'text/html');
-	res.write('<html>')
-	res.write('<head><title>Mi first Page</title></head>')
-	res.write('<body><h1>Hello world from my Server</h1></body>')
-	res.write('</html>')
+	res.write('<html>');
+	res.write('<head><title>Mi first Page</title></head>');
+	res.write('<body><h1>Hello world from my Server</h1></body>');
+	res.write('</html>');
 	res.end();
 });
 
