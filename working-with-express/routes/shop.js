f84e692e65fa975a8c809f0d5220115fec01d549
Arley Sneyder Rico Bohórquez <arleyrc09@gmail.com>
2021-04-19 16:45:29 -0500

    handle file paths to send files

--- working-with-express/routes/shop.js
+++ working-with-express/routes/shop.js
@@ -1,11 +1,14 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 // important: router.get its just for the EXACT path while router.uses is for ALL paths
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
