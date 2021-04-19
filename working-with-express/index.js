6c7b51a3cfcc6a6897a81551f4d5ca18875d94a1
Arley Sneyder Rico Bohórquez <arleyrc09@gmail.com>
2021-04-19 16:38:22 -0500

    Add html files to return in the response

--- working-with-express/index.js
+++ working-with-express/index.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
@@ -8,7 +10,12 @@ const shopRoutes = require('./routes/shop');
 // we  dont need bodyParser.urlencoded()
 app.use(express.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+// by default it puts the path '/'
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3000);
--- working-with-express/routes/shop.js
+++ working-with-express/routes/shop.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 // important: router.get its just for the EXACT path while router.uses is for ALL paths
 router.get('/', (req, res, next) => {
-  res.send('<h1>Hello world form express</h1>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
