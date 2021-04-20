1e72ac3e30e2fc6f52e9c70f57e64f6088658512
Arley Sneyder Rico Bohórquez <arleyrc09@gmail.com>
2021-04-20 12:25:11 -0500

    Add ejs config and files

--- dynamic-content/index.js
+++ dynamic-content/index.js
@@ -1,17 +1,18 @@
 const path = require('path');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 const express = require('express');
 
 const app = express();
 
 // app.set('view engine', 'pug');
-app.engine('hbs', expressHbs({
-  layoutsDir: 'views/layouts/',
-  defaultLayout: 'main-layout',
-  extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+// app.engine('hbs', expressHbs({
+//   layoutsDir: 'views/layouts/',
+//   defaultLayout: 'main-layout',
+//   extname: 'hbs'
+// }));
+// app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
