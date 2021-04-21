9f4953aef2c1f70a6f42be586f095b6deb0aad3d
Arley Sneyder Rico Bohórquez <arleyrc09@gmail.com>
2021-04-21 14:50:46 -0500

    Render product deailt and pass data with POST request

---
+++ 06-dynamic-routes-advanced-models/models/product.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = require('../util/path');
+
+const p = path.join(
+  rootDir,
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.id = Math.floor(Math.random()*20000000000).toString();
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      console.log(products);
+      const product = products.find(p => p.id === id);
+
+      cb(product);
+    })
+  }
+};
