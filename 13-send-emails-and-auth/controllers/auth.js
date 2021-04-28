9aa8971d6c665501a2a3f8e6acb7e68d715dc748
Arley Sneyder Rico Bohórquez <arleyrc09@gmail.com>
2021-04-28 11:52:11 -0500

    Build structure to reset password

--- 13-send-emails-and-auth/controllers/auth.js
+++ 13-send-emails-and-auth/controllers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 const sendGridTransport = require('nodemailer-sendgrid-transport');
 
@@ -12,14 +13,16 @@ const transporter = nodemailer.createTransport(sendGridTransport({
   }
 }));
 
+const getMessage = (message) => {
+  if (message.length > 0) {
+    return message[0];
+  }
+    return null;
+}
+
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
-
-  if (message.length > 0) {
-    message = message[0];
-  } else {
-    message = null;
-  }
+  message = getMessage(message);
 
   res.render('auth/login', {
     path: '/login',
@@ -30,12 +33,7 @@ exports.getLogin = (req, res, next) => {
 
 exports.getSignup = (req, res, next) => {
   let message = req.flash('error');
-
-  if (message.length > 0) {
-    message = message[0];
-  } else {
-    message = null;
-  }
+  message = getMessage(message);
 
   res.render('auth/signup', {
     path: '/signup',
@@ -119,3 +117,68 @@ exports.postLogout = (req, res, next) => {
     res.redirect('/');
   });
 };
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  message = getMessage(message);
+
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
+  })
+}
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+
+    const token = buffer.toString('hex');
+
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash('error', 'No account with that email found.');
+          return res.redirect('/reset');
+        }
+
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then(result => {
+        res.redirect('/');
+
+        return transporter.sendMail({
+          to: req.body.email,
+          from: process.env.SENGRID_SENDER,
+          subject: 'Password Reset',
+          html: `
+            <p>You requested a password reset</p>
+            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+          `
+        })
+      })
+      .catch(err => console.log(err));
+  })
+}
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then(user => {
+      let message = req.flash('error');
+      message = getMessage(message);
+
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'New Password',
+        errorMessage: message,
+        userId: user._id.toString(),
+      })
+    })
+    .catch(err => console.log(err));
+}
