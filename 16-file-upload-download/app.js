425eb4b100b5cda42172716432653749f909115a
Arley Sneyder Rico Bohórquez <arleyrc09@gmail.com>
2021-05-03 09:06:51 -0500

    Serve file statically

--- 16-file-upload-download/app.js
+++ 16-file-upload-download/app.js
@@ -54,6 +54,7 @@ const authRoutes = require('./routes/auth');
 app.use(express.urlencoded({ extended: false }));
 app.use(multer({ storage, fileFilter }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
   session({
     secret: secret,
