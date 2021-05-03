b6246466ca67e55d7e113aab9fe7e37630731880
Arley Sneyder Rico Bohórquez <arleyrc09@gmail.com>
2021-05-03 14:23:18 -0500

    Delete poduct using async requests

---
+++ 18-async-requests/routes/admin.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { body } = require('express-validator');
+
+const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/isAuth');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', isAuth, adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', isAuth, adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', [
+  body('title').isString().isLength({ min: 3 }),
+  body('price').isFloat(),
+  body('description').isLength({ min: 5, max: 200 }),
+], isAuth, adminController.postAddProduct);
+
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
+
+router.post('/edit-product', [
+  body('title').isString().isLength({ min: 3 }),
+  body('price').isFloat(),
+  body('description').isLength({ min: 5, max: 200 }),
+], isAuth, adminController.postEditProduct);
+
+router.delete('/product/:productId', isAuth, adminController.deleteProduct);
+
+module.exports = router;
